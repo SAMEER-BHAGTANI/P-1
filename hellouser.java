df2b9b14d81967f9313c42ce5a1c2df0924a7e13
SAMEER-BHAGTANI <114795359+SAMEER-BHAGTANI@users.noreply.github.com>
2022-10-01 11:02:13 +0530

    Add files via upload

---
+++ hellouser.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class hello
+{
+	public static void main(String[] args)
+	{
+
+		Scanner x=new Scanner(System.in);
+	
+		String name;
+
+		System.out.println("Hello "+name);
+	}
+}
