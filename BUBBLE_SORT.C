855a7bdf4466a73c68da1de51df7e37a3ac99482
SAMEER-BHAGTANI <114795359+SAMEER-BHAGTANI@users.noreply.github.com>
2022-10-04 10:58:41 +0530

    Add files via upload

---
+++ BUBBLE_SORT.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a[5],i,j,temp;
+
+	clrscr();
+
+	for(i=0;i<=4;i++)
+	{
+		printf("\n\t\t\tEnter %d number : ",i+1);
+		
+	}
+
+	for(i=0;i<=4;i++)
+	{
+		for(j=0;j<=i;j++)
+		{
+			
+		}
+	}
+
+
+	printf("\n\n\t\t\tSorted array is : ");
+
+	for(i=0;i<=4;i++)
+	{
+		printf("\n\n\t\t\t%d",a);
+	}
+
+	getch();
+}
